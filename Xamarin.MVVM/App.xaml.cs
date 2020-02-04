c5d850dc2d51de6f57d93be092409f21a04f0507
Ed Zajac <edzajac@Eds-MacBook-Pro.local>
2020-02-04 14:59:06 -0500

    Added MainPageView to app

--- Xamarin.MVVM/App.xaml.cs
+++ Xamarin.MVVM/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.MVVM.Views;
 
 namespace Xamarin.MVVM
 {
@@ -10,7 +11,7 @@ namespace Xamarin.MVVM
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPageView());
         }
 
         protected override void OnStart()
---
+++ Xamarin.MVVM/Views/MainPageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.MVVM.Views
+{
+    public partial class MainPageView : ContentPage
+    {
+        public MainPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
