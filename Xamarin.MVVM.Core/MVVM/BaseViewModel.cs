7bbe1cb06fd43dda703d4680775ffb13dbd52bb7
Ed Zajac <edzajac@Eds-MacBook-Pro.local>
2020-02-04 15:06:52 -0500

    Added MVVM files including BaseView, BaseViewModel, Command override, etc.

---
+++ Xamarin.MVVM.Core/MVVM/BaseViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Xamarin.MVVM.Core.MVVM
+{
+    public class BaseViewModel : INotifyPropertyChanged, IViewModel
+    {
+        private bool _isBusy;
+        private string _title;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public string Title
+        {
+            get => _title;
+            set => RaiseAndUpdate(ref _title, value);
+        }
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                if (RaiseAndUpdate(ref _isBusy, value))
+                {
+                    Raise(nameof(IsNotBusy));
+                }
+            }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public virtual Task InitAsync() => Task.FromResult(true);
+
+        /// <summary>
+        /// Raises the specified property and updates the inputted field with the new value.
+        /// </summary>
+        /// <returns><c>true</c>, if and update was raised, <c>false</c> otherwise.</returns>
+        /// <param name="field">Field.</param>
+        /// <param name="value">Value.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        protected bool RaiseAndUpdate<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            Raise(propertyName);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Raises the specified property.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected void Raise([CallerMemberName] string propertyName = null)
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Xamarin.MVVM.Core/MVVM/Command.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Xamarin.MVVM.Core.MVVM
+{
+    public class Command : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+
+        public Command(Action<object> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public Command(Action execute) : this(o => execute())
+        {
+            if (execute == null)
+            {
+                var nullException = new ArgumentNullException(nameof(execute));
+
+                throw nullException;
+            }
+        }
+
+        public Command(Action<object> execute, Func<object, bool> canExecute) : this(execute)
+        {
+            if (canExecute != null)
+            {
+                _canExecute = canExecute;
+            }
+            else
+            {
+                var nullException = new ArgumentNullException(nameof(canExecute));
+
+                throw nullException;
+            }
+        }
+
+        public Command(Action execute, Func<bool> canExecute) : this(o => execute(), o => canExecute())
+        {
+            if (execute == null)
+            {
+                var nullException = new ArgumentNullException(nameof(execute));
+
+                throw nullException;
+            }
+
+            if (canExecute == null)
+            {
+                var nullException = new ArgumentNullException(nameof(canExecute));
+
+                throw nullException;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(parameter);
+            }
+
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void ChangeCanExecute()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
